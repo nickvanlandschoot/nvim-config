11962a5bb4a5c52de3a8ad749907a9bfc11021f5
Nicholas Van Landschoot <nicholas@apigen.com>
2025-02-17 19:54:01 -0700

    implement more lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,18 +11,49 @@ return {
       require("mason-lspconfig").setup({
 
 	--Install different LSPs
-	ensure_installed = { "lua_ls", "ts_ls" }
+	ensure_installed = { "lua_ls", "ts_ls", "pyright", "yamlls", "jsonls" }
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
-    config = function()
+    dependencies = { 'saghen/blink.cmp' },
+
+    servers = {
+      lua_ls = {},
+      ts_ls = {},
+      pyright = {},
+      yaamlls = {},
+      jsonls = {},
+    },
+
+    config = function(_, servers)
       local lspconfig = require("lspconfig")
+
+      for server, config in pairs(servers) do
+	local capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+	config.capabilities = capabilities
+	lspconfig[server].setup(config)
+
+      end
 
       --Setup LSPs
       lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.yamlls.setup({})
+      lspconfig.jsonls.setup({})
+
+      lspconfig.ts_ls.setup({
+	filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+	on_attach = function(client, bufnr)
+	  -- If using a separate formatter (like prettier), disable tsserver's formatting:
+	  client.server_capabilities.documentFormattingProvider = false
+	  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	end,
+      })
+
       --Custom keybindings
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
