94fe372b2fbea9c424193da10878a62d21f8e058
Nicholas Van Landschoot <nicholas@apigen.com>
2025-02-24 14:54:54 -0700

    telescope changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,20 +8,22 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
+        defaults = {
+          hidden = false,
+        },
         extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-	  hidden=false
+          ["ui-select"] = require("telescope.themes").get_dropdown({}),
         },
       })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-      vim.keymap.set('n', '<leader>f', builtin.current_buffer_fuzzy_find, {})
+      vim.keymap.set("n", "<leader>f", builtin.current_buffer_fuzzy_find, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>f.", builtin.lsp_document_symbols, {})
       require("telescope").load_extension("ui-select")
-
     end,
   },
 }
+
