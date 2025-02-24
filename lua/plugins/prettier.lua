3d2a078978f36934a6aefb301c5416446e071500
Nicholas Van Landschoot <nicholas@apigen.com>
2025-02-24 14:54:34 -0700

    prettier changes

--- lua/plugins/prettier.lua
+++ lua/plugins/prettier.lua
@@ -1,10 +1,28 @@
 return {
-  "prettier/vim-prettier",
-  build = "npm install",
-  ft = { "javascript", "typescript", "json", "css", "html" }, -- add filetypes as needed
+  'sindrets/diffview.nvim',
+  requires = { 'nvim-lua/plenary.nvim' },
   config = function()
-    -- Optionally enable/disable autoformat on save
-    vim.g["prettier#autoformat"] = 1  -- or set to 0 if you don't want autoformatting
-  end,
+    require("diffview").setup({
+      diff_binaries = false,         -- Skip binary files in diffs
+      enhanced_diff_hl = true,       -- Better diff highlighting
+      use_icons = true,             -- Enable icons for a more intuitive interface
+      file_panel = {
+        listing_style = "tree",     -- File panel listing style
+        tree_options = {
+          flatten_dirs = true,      -- Flatten directory structure for easier navigation
+        },
+        win_config = {
+          position = "left",        -- Position of the file panel window
+          width = 35,
+        },
+      },
+      commit_log_panel = {
+        win_config = {
+          position = "bottom",      -- Position of the commit log panel
+          height = 16,
+        },
+      },
+    })
+  end
 }
 
